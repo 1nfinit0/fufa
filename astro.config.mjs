0d65f0d60d041e06d7aa05d844a04687efe126f7
Tobi <noggnzzz@gmail.com>
2026-02-07 06:44:34 -0500

    home and slugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static'
+  output: 'static',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
